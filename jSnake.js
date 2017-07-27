8ac9946907bb831ee65527dafff55dd8b009eb9a
Walter Johnson <waltercj3@gmail.com>
2017-07-26 22:23:46 -0700

    26jul17 good progress through slither

--- jSnake.js
+++ jSnake.js
@@ -10,20 +10,24 @@ var JSG = {
   width: 0, //JSG.cvs.width,
   height: 0, //JSG.cvs.height,
   snake: [],
-  prey: [],
+  prey: {},
   snakeColor: "DimGrey",
   preyColor: "Brown",
-  square: 16,
-  gridHorz: 0, //Math.floor(JSG.width/JSG.square),
-  gridVert: 0, //Math.floor(JSG.height/JSG.square),
-  ranLoc: null,
-  drawSq: null, 
+  square: 16, //size of the grid squares
+  gridHorz: 0, //Math.floor(JSG.width/JSG.square), //number of squares across
+  gridVert: 0, //Math.floor(JSG.height/JSG.square), //number of squares vertically
+  direction: "not", //set by arrow keys: north, south, east, and west
+  ranLoc: null, //random location function
+  drawSq: null, // draws squares that makes up snake and prey
   drawPrey: null,
-  drawSnake:null,
-  init: null
+  drawSnake: null,
+  drawBorder: null,
+  setDirListener: null,
+  slither: null, //moves snake
+  init: null //everything needed tp start
 };
 
-JSG.ranLoc = function() {
+JSG.ranLoc = function () {
   var x, y, loc;
   x = Math.floor(Math.random() * JSG.gridHorz);
   y = Math.floor(Math.random() * JSG.gridVert);
@@ -31,7 +35,7 @@ JSG.ranLoc = function() {
   return loc;
 };
 
-JSG.drawSq = function(gridLoc, type) {
+JSG.drawSq = function (gridLoc, type) {
   var x, y, size = JSG.square - 2,
     color = (type === "snake") ? JSG.snakeColor : JSG.preyColor;
   x = gridLoc.x * JSG.square;
@@ -42,16 +46,16 @@ JSG.drawSq = function(gridLoc, type) {
   JSG.ctx.restore();
 };
 
-JSG.clearSq = function(gridLoc) {
-  var x, y, size = JSG.square - 2;
-  x = gridLoc.x * JSG.square;
-  y = gridLoc.y * JSG.square;
+JSG.clearSq = function (gridLoc) {
+  var x, y, size = JSG.square;
+  x = (gridLoc.x * JSG.square) - 1;
+  y = (gridLoc.y * JSG.square) - 1;
   JSG.ctx.save();
   JSG.ctx.clearRect(x, y, size, size);
   JSG.ctx.restore();
 };
 
-JSG.drawPrey = function() {
+JSG.drawPrey = function () {
   var i, sn = JSG.snake.length,
     location = JSG.ranLoc();
   for (i = 0; i < sn; i += 1) {
@@ -61,17 +65,15 @@ JSG.drawPrey = function() {
     }
   }
   JSG.drawSq(location, "prey");
+  JSG.prey = location;
 };
 
-JSG.drawSnake = function(location) {
-  var length = JSG.snake.unshift(location);
-  if (length > 1) {
-    JSG.clearSq(JSG.snake[length - 1]);
-  }
+JSG.drawSnake = function (location) {
+  JSG.snake.unshift(location);
   JSG.drawSq(location, "snake");
 };
 
-JSG.drawBorder = function() {
+JSG.drawBorder = function () {
   JSG.ctx.save();
   JSG.ctx.beginPath;
   JSG.ctx.moveTo(0, 0);
@@ -84,7 +86,53 @@ JSG.drawBorder = function() {
   JSG.ctx.restore();
 };
 
-JSG.init = function() {
+JSG.setDirListener = function () {
+  var keycode = 0, 
+    KEY_CODES = {
+      37: "left",
+      38: "up",
+      39: "right",
+      40: "down"
+    };
+
+  document.addEventListener("keydown", function(e) {
+    keycode = (e.keyCode) ? e.keyCode : e.charCode;
+    if(KEY_CODES[keycode]) {
+      e.preventDefault();
+      JSG.direction = KEY_CODES[keycode];
+    }
+  });
+};
+
+JSG.slither = function () {
+  var next = {}, 
+    head = JSG.snake[0];
+  switch(JSG.direction) {
+    case "left":
+      next = {x: (head.x === 0 ? JSG.gridHorz : head.x - 1), y: head.y};
+      break;
+    case "up":
+      next = {x: head.x, y: (head.y === 0 ? JSG.gridVert : head.y - 1)};
+      break;
+    case "right":
+      next = {x: (head.x === JSG.gridHorz ? 0 : head.x + 1), y: head.y};
+      break;
+    case "down":
+      next = {x: head.x, y: (head.y === JSG.gridVert ? 0 : head.y + 1)};
+      break;
+    default:
+      next = {x: head.x, y: head.y};
+  }
+  
+  //remove and erase tail of snake unless the snake eats prey and grows
+  if(!(next.x === JSG.prey.x && next.y === JSG.prey.y)) {
+    JSG.clearSq(JSG.snake.pop());
+  }
+
+  JSG.drawSnake(next);
+};
+
+JSG.init = function () {
   JSG.ctx = JSG.cvs.getContext("2d");
   JSG.width = JSG.cvs.width;
   JSG.height = JSG.cvs.height;
@@ -93,8 +141,11 @@ JSG.init = function() {
   JSG.drawBorder();
   JSG.drawSnake(JSG.ranLoc());
   JSG.drawPrey();
+  JSG.setDirListener();
 };
 
-window.onload = function() {
+window.onload = function () {
   JSG.init();
+  JSG.setDirListener();
 };
+
